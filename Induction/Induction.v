4c0f9ccddf30eb212618d6e4d8cfaafa1ed5ac1a
Fedor Sakharov <fedor.sakharov@gmail.com>
2013-11-02 13:58:24 +0400

    Induction: Adds solutions to basic_induction.

--- Induction/Induction.v
+++ Induction/Induction.v
@@ -51,3 +51,59 @@ Proof.
     trivial.
     trivial.
 Qed.
+
+Theorem plus_0_r : forall n:nat, n + 0 = n.
+Proof.
+    intros n. induction n as [| n'].
+    Case "n = 0". reflexivity.
+    Case "n = S n'". simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem minus_diag : forall n, minus n n = 0.
+Proof.
+    intros n. induction n as [| n'].
+    Case "n = 0".
+        simpl. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem mult_0_r : forall n : nat, n * 0 = 0.
+Proof.
+    intros n. induction n as [| n'].
+    Case "n = 0".
+        simpl. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat,
+      S (n + m) = n + (S m).
+Proof.
+    intros n m. induction n as [| n'].
+    Case "n = 0".
+        simpl. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+
+Theorem plus_comm : forall n m : nat,
+      n + m = m + n.
+Proof.
+    intros n m. induction n as [| n'].
+    Case "n = 0".
+        simpl. rewrite -> plus_0_r. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. rewrite -> plus_n_Sm. reflexivity.
+Qed.
+
+Theorem plus_assoc : forall n m p : nat,
+      n + (m + p) = (n + m) + p.
+Proof.
+    intros n m p. induction n as [| n'].
+    Case "n = 0".
+        simpl. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. reflexivity.
+Qed.
