7927acbcbc337c08ae2aa2d59933971b3fa44b05
Fedor Sakharov <fedor.sakharov@gmail.com>
2013-11-06 18:08:50 +0400

    MoreCoq: Removes nat definition and adds solution to beq_nat_true.

--- MoreCoq/MoreCoq.v
+++ MoreCoq/MoreCoq.v
@@ -96,10 +96,6 @@ Proof.
     apply eq1.
 Qed.
 
-Inductive nat : Type :=
-    | O : nat
-    | S : nat -> nat.
-
 Theorem eq_add_S : forall(n m : nat),
     S n = S m ->
     n = m.
@@ -162,14 +158,13 @@ Proof.
     reflexivity.
 Qed.
 
-(*
 Theorem length_snoc' : forall(X : Type) (v : X)
     (l : list X) (n : nat),
     length l = n ->
     length (snoc l v) = S n.
 Proof.
     intros X v l. induction l as [| v' l'].
-    Case "l = []". intros n eq. rewrite ← eq. reflexivity.
+    Case "l = []". intros n eq. rewrite <- eq. reflexivity.
     Case "l = v' :: l'". intros n eq. simpl. destruct n as [| n'].
     SCase "n = 0". inversion eq.
     SCase "n = S n'".
@@ -178,5 +173,87 @@ Qed.
 
 Theorem beq_nat_0_l : forall n,
     beq_nat 0 n = true -> n = 0.
+Proof.
+    intros n eq1.
+    induction n as [| n'].
+    Case "n = 0".
+        reflexivity.
+    Case "n = S n'".
+        inversion eq1.
+Qed.
 
-*)
+Theorem beq_nat_0_r : forall n,
+    beq_nat n 0 = true -> n = 0.
+Proof.
+    intros n eq1.
+    induction n as [| n'].
+    Case "n = 0".
+        reflexivity.
+
+    Case "n = S n'".
+        inversion eq1.
+Qed.
+
+Theorem S_inj : forall(n m : nat) (b : bool),
+    beq_nat (S n) (S m) = b ->
+    beq_nat n m = b.
+Proof.
+    intros n m b H.
+    simpl in H.
+    apply H.
+Qed.
+
+Theorem silly3' : forall (n : nat),
+    (beq_nat n 5 = true -> beq_nat (S (S n)) 7 = true) ->
+        true = beq_nat n 5 ->
+        true = beq_nat (S (S n)) 7.
+Proof.
+    intros n eq H.
+    symmetry in H. apply eq in H. symmetry in H.
+    apply H.
+Qed.
+
+Theorem plus_n_n_injective : forall n m,
+    n + n = m + m ->
+    n = m.
+Proof.
+    intros n. induction n as [| n'].
+    intros.
+    simpl in H.
+    destruct m.
+        reflexivity.
+        inversion H.
+
+    intros.
+    destruct m.
+    inversion H.
+    simpl in H.
+    rewrite <- plus_n_Sm in H.
+    rewrite <- plus_n_Sm in H.
+    apply eq_add_S in H.
+    apply eq_add_S in H.
+    assert (n' = m) as Heq. apply IHn'. assumption.
+    rewrite Heq.
+    reflexivity.
+Qed.
+
+Theorem beq_nat_true : forall n m,
+    beq_nat n m = true -> n = m.
+Proof.
+    intros n.
+    induction n as [| n'].
+    intros m eq.
+    destruct m.
+    reflexivity.
+
+    inversion eq.
+
+    intros m eq.
+    destruct m as [| m'].
+    inversion eq.
+
+    apply f_equal.
+    apply IHn'.
+    inversion eq.
+Qed.
+
